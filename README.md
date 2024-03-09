99efac3ac3010a4eb161caa6a39bb4b647e124f1
NongTarNarak <pakkaphan.per@student.mahidol.edu>
2024-03-10 00:18:11 +0700

    First finish prototype

---
+++ README.md
@@ -0,0 +1,35 @@
+Secure Image System
+Welcome to the Secure Image System, powered by AES encryption and SHA-256 hashing.
+
+How to Use:
+>>Option 1: Encrypt an Image
+    1) Upload the image you want to encrypt to the folder named "PlainText_image"
+    2) Choose option "1" and provide the path to your image
+    3) Name your image
+    4) Specify the image type (you can choose any type you prefer)
+    5) Set your password for encrypting the image (please remember this password as it will be needed for decrypting)
+
+>>Option 2: Decrypt an Image
+    2.1): Decrypt from Local Storage
+        2.1.1) Enter the name of the image you want to decrypt
+        2.1.2) Provide the password used for encryption
+        2.1.3) The decrypted image will be saved to the folder named "Decrypted_image"
+    2.2): Decrypt from Uploaded JSON
+        2.2.1) Enter the image name
+        2.2.2) Use the default password "ICT555"
+        2.2.3) Proceed as above
+
+>Option 3: List All Cipher Images
+    View a list of all cipher images on your system, including their names, password lengths, and whether they were uploaded or encrypted.
+
+>Option 4: Export Cipher Images to JSON
+    Export cipher images to a JSON file for safekeeping. You can decrypt these images in the future by uploading them into the system (Option 5).
+
+>>Option 5: Insert Cipher Images (JSON) into the System
+    5.1): Upload JSON File
+        5.1.1) Place the .json file in the "CipherText" folder
+        5.1.2) Provide the path to your JSON file
+    5.2): Decrypt and Import JSON File
+        5.2.1) Place the .json file in the appropriate folder
+        5.2.2) Provide the path to your JSON file
+        5.2.3) The decrypted image will be saved to the "Decrypted_image" folder on your system
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 from hashlib import sha256
-import time;
+import time # use for delaying the program
+import json # use for import and export json file format
+# Cipher text and Key from AES is bytes type and when we export that we must change into string type
+# because json cannot contain bytes format type
+# and the problem occurs when we want to change the string into the original bytes format
+# so we use module called "ast" to change string into bytes type.
+import ast #use for parse the string to bytes
 
 def generate_key(password):
     """
@@ -22,7 +28,6 @@ def encrypt_image(image_path, password):
     
     # Padding plaintext to match block size
     padded_plaintext = pad(plaintext, AES.block_size)
-
     ciphertext = cipher.iv + cipher.encrypt(padded_plaintext)
 
     return key, ciphertext
@@ -42,52 +47,60 @@ def verify_password(name,passwd,allitems):
         package = allitems[name];
         if(package is not None):
             if(package[2] == passwd):
+                print();
                 print("** Authentication Successful **");
+                print();
+                time.sleep(1);
                 return True;
             else:
+                print();
                 print("** Password is not correct **");
+                print();
+                time.sleep(1);
                 return False;
     except:
+        print();
         print(f"** There are not images named '{name}' **");
+        print();
+        time.sleep(1);
+        return False;
 
 def verify_encrpyt_input(image_path,image_file_type):
     whitelistFileType = ['.jpeg','.jpg','.png','.bmp'];
     path = True;
     filetype = True;
-    
+
     # CHECK PATH
     try:
         with open(image_path, 'rb') as f:
             plaintext = f.read()
     except:
         path = False;
-        time.sleep(1);
         print();
-        print(">> ** Path not Avaliable **");print();
+        print(">> ** Path not Avaliable **");
+        print();
+        time.sleep(1);
     
     if(image_file_type not in whitelistFileType):
-        time.sleep(1);
         print();
-        print(">> ** File type Not Avaliable **");print();
+        print(">> ** File type Not Avaliable **");
+        print();
+        time.sleep(1);
         filetype = False;
     return (path and filetype);
     
 
 def list_image_details(allEncryptImageDict):
-    # Find the maximum lengths for each column
-    max_name_length = max(len(name) for name in allEncryptImageDict.keys())
-    max_password_length = max(len(details[2]) for details in allEncryptImageDict.values())
-    max_file_type_length = max(len(details[3]) for details in allEncryptImageDict.values())
-
+    uploadcheck = (details[4] for details in allEncryptImageDict.values())
 
     # Print header
-    print(f"+{'-' * (105)}+")
-    print(f"| {' ' * 5}{'Image Name':^{23}}{' ' * 5} | {' ' * 5}{'Password Length':^{23}}{' ' * 5} | {' ' * 5}{'Image File Type':^{21}}{' ' * 5} |")
-    print(f"+{'-' * (105)}+")
+    print(f"+{'-' * (115)}+")
+    print(f"| {' ' * 5}{'Image Name':^{23}}{' ' * 5} | {' ' * 5}{'Password Length':^{23}}{' ' * 5} | {' ' * 5}{'Image File Type':^{21}}{' ' * 5} | {'Upload':^{7}} |")
+    print(f"+{'-' * (115)}+")
 
     # Print rows
     for name, details in allEncryptImageDict.items():
-        key, ciphertext, password, image_file_type = details
+        key, ciphertext, password, image_file_type, uploadcheck = details
         
         # if length of name is longer than table
         if(len(name) > 33):
@@ -98,14 +111,23 @@ def list_image_details(allEncryptImageDict):
             # set constant of '*' if it overflow
             masked_password = '*' * 34;
             masked_password = masked_password[:-11] + "..." + " (" + str(len(password)) + ")";
-            
         
-        print(f"| {name:^{33}} | {masked_password:^{33}} | {image_file_type:^{31}} |")
-
+        if(uploadcheck == "Y"):
+            print(f"| {name:^{33}} | {masked_password:^{33}} | {image_file_type:^{31}} | {uploadcheck:^{7}} |");
+        else:
+            print(f"| {name:^{33}} | {masked_password:^{33}} | {image_file_type:^{31}} | {uploadcheck:^{7}} |");
+            
     # Print bottom border
-    print(f"+{'-' * (105)}+");
+    print(f"+{'-' * (115)}+");
     
-    
+
+def convert_string_to_bytes(byte_str):
+    # Using ast.literal_eval() to evaluate the string as a Python expression
+    byte_data = ast.literal_eval(byte_str)
+
+    return byte_data
+
+
 # MAIN ZONE
 if __name__ == "__main__":
     allEncryptImageDict = {};
@@ -118,7 +140,8 @@ if __name__ == "__main__":
         print('Type 1: Encrypt the image with AES.');
         print('Type 2: Decrypt the image.');
         print('Type 3: List all encrpyted image name.');
-        print('Type 4: Show Cipher Text and Key.');
+        print('Type 4: Export Cipher image and Key.');
+        print('Type 5: Insert Cipher image to the system.')
         print('Type x: EXIT');
         
         
@@ -126,16 +149,18 @@ if __name__ == "__main__":
         whitelistchoice = ['1','2','3','x','X'];
         
         if(x == '1'):
-            image_path = str(input('Put your image path here >>'));
+            image_path = str(input('Put your image path here >> ./PlainText_image/'));
             image_name = str(input('Image name >>'));
             image_file_type = str(input('Image type (.png .jpeg .jpg .bmp) >>'));
             password = str(input('Password For Master Key (Generate by SHA256) [Must remember] >>'));
+            upload = "";
             
             # this step we verify some of input
+            image_path = "./PlainText_image/"+image_path
             if(verify_encrpyt_input(image_path,image_file_type)):
     
                 key, ciphertext = encrypt_image(image_path, password); 
-                tempObject = [key,ciphertext,password,image_file_type];
+                tempObject = [key,ciphertext,password,image_file_type,upload];
                 # When it's time to decrypt just use Key as index 0, Ciphertext as index 1
                 
                 # set image_name as key, set tempObject as values contain all of importance data
@@ -152,18 +177,31 @@ if __name__ == "__main__":
                 
         elif(x == '2'):
             image_name = str(input("Image's name for decryption >>"));
-            password_decrypt = str(input("Password >>"));
+            password_decrypt = str(input("password >>"));
             
             #Authenticate user with their image before decrypt
             if(verify_password(image_name,password_decrypt,allEncryptImageDict)):
+                objectyouwanttoDecrypt = allEncryptImageDict[image_name]
+                
+                # always change it into bytes by using .encode("utf-8")
+                key, ciphertext = objectyouwanttoDecrypt[0],objectyouwanttoDecrypt[1];
+                # print(key);
+                # x = str(key);
+                
+                if(type(key) != bytes or type(ciphertext) != bytes):
+                    # In json cipher text and key are in the wrong format
+                    # so we import ast module to change string to the original bytes
+                    key, ciphertext = convert_string_to_bytes(key),convert_string_to_bytes(ciphertext)
+                
+                # print(key);
                 # Decrypting data
                 decrypted_data = decrypt_image(ciphertext, key);
 
                 # Writing decrypted data to a new image file
-                outputfilename = image_name+allEncryptImageDict[image_name][3];
+                outputfilename =  image_name+allEncryptImageDict[image_name][3];
                 
-                with open(outputfilename, 'wb') as f:
-                    f.write("./Decrypted_image"+decrypted_data);
+                with open("./Decrypted_image/"+outputfilename, 'wb') as f:
+                    f.write(decrypted_data);
                 print("Image decrypted successfully.")
             else:
                 # If authenticate failed
@@ -178,6 +216,68 @@ if __name__ == "__main__":
             continue;
         
         elif(x == '4'):
+            print();
+            print(f">> In this step we'll write out the encrypted file to .json file in folder (CipherText) as well the key.")
+            print(f">> then if you want to decrypt that image you must type 5")
+            
+            image_name = str(input("Image's name to write out >>"));
+            password_writefileout = str(input("password >>"));
+            
+            
+            if(verify_password(image_name,password_writefileout,allEncryptImageDict)):
+                object_to_write_out = allEncryptImageDict[image_name];
+                
+                key, ciphertext, password, image_file_type, upload = object_to_write_out;
+                
+                
+                data = {
+                    "name": image_name,
+                    "cipher": str(ciphertext),
+                    "filetype": image_file_type,
+                    "key": str(key)
+                }
+
+                # File path to write JSON data
+                file_path = "CipherText/"+image_name+".json"
+
+                # Writing data to JSON file
+                with open(file_path, "w") as json_file:
+                    json.dump(data, json_file)
+
+                print("Data has been written to", file_path)
+                
+            else:
+                # If authenticate failed
+                continue;
+            
+        elif(x == '5'):
+            print("Option 1: Insert to system only");
+            print("Option 2: Insert to system && decrypt to Decrypted_image folder");
+            option = str(input(">>"));
+            
+            # File path of the JSON file to read
+            file_path = str(input('JSON file path remember to put .json >> ./CipherText/'));
+
+            file_path = "CipherText/"+file_path
+            # Reading data from JSON file
+            with open(file_path, "r") as json_file:
+                data = json.load(json_file)
+            
+            
+            name,cipherdata,filetype,key = data["name"],data["cipher"],data["filetype"],data["key"];
+            
+            
+            # add upload signature set default password for upload to ICT555
+            tempObject = [key,cipherdata,"ICT555",filetype,"Y"];
+            # When it's time to decrypt just use Key as index 0, Ciphertext as index 1
+            
+            
+            # set image_name as key, set tempObject as values contain all of importance data
+            allEncryptImageDict[name] = tempObject;
+            
+            if(option == "2"):
+                print('decrypting')
+            
             continue;
         
         elif(x == 'x'):
